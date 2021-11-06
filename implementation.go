e22016060a045e02fe3a76cf2da11c47585936cf
lightballer <bokiiartem@gmail.com>
2021-11-06 20:42:45 +0200

    implement postfix calculation

--- implementation.go
+++ implementation.go
@@ -1,9 +1,45 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func CalculatePostfix(input string) (string, error) {
+	var stack Stack
+	for i := 0; i < len(input); i++ {
+		currentChar := input[i : i+1]
+		if currentChar == " " {
+			continue
+		}
+		if parsed, err := strconv.ParseFloat(currentChar, 64); err == nil {
+			stack.Push(parsed)
+		} else {
+			second, _ := stack.Pop()
+			first, _ := stack.Pop()
+			if calcResult, err := DoOperation(currentChar, first, second); err == false {
+				stack.Push(calcResult)
+			}
+		}
+	}
+	result, _ := stack.Pop()
+	return fmt.Sprintf("%f", result), nil
+}
+
+func DoOperation(operation string, firstOperand, secondOperand float64) (float64, bool) {
+	switch operation {
+	case "+":
+		return firstOperand + secondOperand, false
+	case "-":
+		return firstOperand - secondOperand, false
+	case "*":
+		return firstOperand * secondOperand, false
+	case "/":
+		return firstOperand / secondOperand, false
+	case "^":
+		return math.Pow(firstOperand, secondOperand), false
+	default:
+		return 0, true
+	}
 }
