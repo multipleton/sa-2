acd6ecffb1fd41faed9e58ec1fd138dd6dce6023
olegmak02 <olleglion02@gmail.com>
2021-11-08 15:20:50 +0200

    added fields in handler.go

--- handler.go
+++ handler.go
@@ -1,9 +1,12 @@
 package lab2
 
+import "io"
+
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
 // Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
+	Input io.Reader
+	Output io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
