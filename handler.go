534578ab38bf1cbf091df0832e7b1cc9eae6f3b5
Yehor Bublyk <yehor.bk@gmail.com>
2021-11-08 19:55:02 +0200

    flag and io handling functionality implemented

--- handler.go
+++ handler.go
@@ -1,12 +1,23 @@
 package lab2
 
-// ComputeHandler should be constructed with input io.Reader and output io.Writer.
-// Its Compute() method should read the expression from input and write the computed result to the output.
+import (
+	"io"
+)
+
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
+	io.Reader
+	io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
-	// TODO: Implement.
-	return nil
+	input := make([]byte, 64)
+	for {
+		_, err := ch.Reader.Read(input)
+		if err == io.EOF {
+			break
+		}
+	}
+	expression := string(input)
+	_, err := CalculatePostfix(expression)
+	return err
 }
